9d7b9062423a92efb4ac13f71130a9102a011da9
NicolaiLoeffler <nicolai.loeffler92@gmail.com>
2016-01-16 13:56:37 +0100

    add ChatCtrl with mocked messages

---
+++ www/js/ctrl/ChatCtrl.js
@@ -0,0 +1,25 @@
+/**
+ * Created by niloeffl on 17.04.15.
+ */
+
+angular.module('soga')
+    .controller('ChatCtrl', function($scope) {
+
+        $scope.test = 'hallo'
+
+        $scope.messages = [
+            {
+              content: 'Hallo',
+              user: 'Nico'
+            },
+            {
+              content: 'Test',
+              user: 'System'
+            },
+            {
+              content: 'jooooooooooooooo',
+              user: 'dummy'
+            }
+        ];
+
+    });
