f5ecbff4263d5838a6d26ac4a592eb5638a0052c
NicolaiLoeffler <nicolai.loeffler92@gmail.com>
2016-01-27 00:31:12 +0100

    display popup if no username defined

--- www/js/ctrl/LoginCtrl.js
+++ www/js/ctrl/LoginCtrl.js
@@ -1,9 +1,16 @@
 angular.module('soga')
-    .controller('LoginCtrl', function($scope, $rootScope, $state) {
+    .controller('LoginCtrl', function($scope, $rootScope, $state, $ionicPopup) {
 
         $scope.login = function(username) {
+            if(username === '' || username == undefined) {
+              var alertPopup = $ionicPopup.alert({
+                  title: 'Username missing',
+                  template: 'Please enter a username!'
+              });
+              return;
+            }
             $rootScope.username = username;
-            $state.go('menu.chat');
+            $state.go('menu.garden');
         };
-        
+
     });
